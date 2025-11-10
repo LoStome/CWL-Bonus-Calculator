4ce9fe08055b60ade8e147e63228d82196b42bc5
Stome <lor.stomeo@gmail.com>
2025-11-10 14:36:25 +0100

    Codice formattato per migliore leggibilita' ed aggiunto il metodo per estrarre le war corrette

--- coc.js
+++ coc.js
@@ -1,25 +1,24 @@
 // LIBRERIE
-require('dotenv').config()
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 //const clashApi = require('clashofclans.js')
 
 // API KEYS
-const COC_API_KEY = process.env.COC_API_TOKEN //official CoC api 
+const COC_API_KEY = process.env.COC_API_TOKEN; //official CoC api
 
 // API URLs
-const CLASHKING_BASE_URL = 'https://api.clashk.ing'; 
-const OFFICIAL_COC_API_URL= 'https://api.clashofclans.com/v1/';
-
+const CLASHKING_BASE_URL = "https://api.clashk.ing";
+const OFFICIAL_COC_API_URL = "https://api.clashofclans.com/v1/";
 
 //INSTANZE API
-//OFFICIAL COC API 
+//OFFICIAL COC API
 const officialCocClient = axios.create({
-    baseURL:OFFICIAL_COC_API_URL,
-    headers: {
-        'Authorization': `Bearer ${COC_API_KEY}`,
-        'Accept': 'application/json',
-        'User-Agent': 'CWL Bonus Calculator (Discord: lo_stome)' 
-    }
+  baseURL: OFFICIAL_COC_API_URL,
+  headers: {
+    Authorization: `Bearer ${COC_API_KEY}`,
+    Accept: "application/json",
+    "User-Agent": "CWL Bonus Calculator (Discord: lo_stome)",
+  },
 });
 
 //CLASH KING API
@@ -28,38 +27,36 @@ const officialCocClient = axios.create({
 const clashKingClient = axios.create({
   baseURL: CLASHKING_BASE_URL,
   headers: {
-    'Accept': 'application/json',
-    'User-Agent': 'CWL Bonus Calculator (Discord: lo_stome)' 
-  }
+    Accept: "application/json",
+    "User-Agent": "CWL Bonus Calculator (Discord: lo_stome)",
+  },
 });
-
 //FINE INSTANZE API
-
 
 // TAGS PER PRODUZIONE
 //note: i tag per l'API kings devono essere senza '#' mentre per l'API ufficiale devono comprendere "#" e poi essere codificate con encodeURIComponent(tag)
-const C_ItalianArmyTag = '2RPVPQLYJ';
-const P_Lore = 'C0UUYY2R';
-const CWL_IAtag= '8QGGJQ8CY'; 
-
+const C_ItalianArmyTag = "2RPVPQLYJ";
+const P_Lore = "C0UUYY2R";
+const CWL_IAtag = "8QGGJQ8CY";
 
 //METODI DI PROVA
-
 //API UFFICIALE
-
 async function showPlayerInfo(playerTag) {
-
-    console.log("player tag passato= "+playerTag)
+  console.log("player tag passato= " + playerTag);
   try {
     //transformo il tag perche' uso l'API UFFICIALE
-    const response = await officialCocClient.get(`/players/${transformTag(playerTag)}`);
+    const response = await officialCocClient.get(
+      `/players/${transformTag(playerTag)}`
+    );
     console.log("Player CoC API Ufficiale:", response.data);
     return response.data;
   } catch (error) {
-    console.error(`Errore nel recupero dati del giocatore:\n ERROR MSG =`, error.response?.data || error.message);
+    console.error(
+      `Errore nel recupero dati del giocatore:\n ERROR MSG =`,
+      error.response?.data || error.message
+    );
   }
 }
-
 
 //API KINGS
 /*
@@ -74,113 +71,117 @@ async function ShowClanInfo(tag) {
 }
 */
 
-
 async function getCwlCurrentSeason(clanTag) {
   try {
     const response = await clashKingClient.get(`/cwl/${clanTag}/group`);
     console.log("Info gruppo CWL:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Errore nel recupero dati della CWL:"+"\n ERROR MSG =", error.response?.data || error.message);
+    console.error(
+      "Errore nel recupero dati della CWL:" + "\n ERROR MSG =",
+      error.response?.data || error.message
+    );
   }
 }
 
-
 async function getCurrentSeasonCWLWarTags(clanTag) {
-    try {
-        //gets API data
-        let response = await clashKingClient.get(`/cwl/${clanTag}/group`);
-        let cwlData = response.data; 
+  try {
+    //gets API data
+    let response = await clashKingClient.get(`/cwl/${clanTag}/group`);
+    let cwlData = response.data;
 
-        let allWarTags = [];
+    let allWarTags = [];
 
-        cwlData.data.rounds.forEach(function(round, roundIndex) {
+    cwlData.data.rounds.forEach(function (round, roundIndex) {
+      //Creates an object for each round with its WarTags in it
+      let roundData = {
+        roundNumber: roundIndex + 1,
 
-            //Creates an object for each round with its WarTags in it
-            let roundData = {
-                roundNumber: roundIndex + 1,
-                
-                //warTags: round.warTags.filter(warTag => warTag && warTag !== '#0')
-                warTags: round.warTags
-                        //#0 are invalid war for CoC's API, so they are filtered out
-                        .filter(warTag => warTag && warTag !== '#0')
-                        // removes # to standardize all tags
-                        .map(warTag => warTag.replace('#', ''))
-                
-            };
-            //console.log("Round creato:", roundData.roundNumber); 
-            allWarTags.push(roundData);
-        });
-        return allWarTags;
-
-    } catch (error) {
-         console.error("Errore nel recupero dati CWL:"+"\n ERROR MSG =", error.response?.data || error.message);
-    }
+        //warTags: round.warTags.filter(warTag => warTag && warTag !== '#0')
+        warTags: round.warTags
+          //#0 are invalid war for CoC's API, so they are filtered out
+          .filter((warTag) => warTag && warTag !== "#0")
+          // removes # to standardize all tags
+          .map((warTag) => warTag.replace("#", "")),
+      };
+      //console.log("Round creato:", roundData.roundNumber);
+      allWarTags.push(roundData);
+    });
+    return allWarTags;
+  } catch (error) {
+    console.error(
+      "Errore nel recupero dati CWL:" + "\n ERROR MSG =",
+      error.response?.data || error.message
+    );
+  }
 }
 
 //Ritorna i dati della war con war tag associato
 async function getWarData(warTag) {
-    try {
-        //transformo il tag perche' uso l'API UFFICIALE
-        let response = await officialCocClient.get(`/clanwarleagues/wars/${transformTag(warTag)}`);
-        let warData = response.data; 
-        console.log("Dati della War "+ warTag, warData);
-        return warData
-    } catch (error) {
-         console.error("Errore nel recupero dati della War "+ warTag+"\n ERROR MSG =", error.response?.data || error.message);
-    }
+  try {
+    //transformo il tag perche' uso l'API UFFICIALE
+    let response = await officialCocClient.get(
+      `/clanwarleagues/wars/${transformTag(warTag)}`
+    );
+    let warData = response.data;
+    //console.log("Dati della War "+ warTag, warData);
+    return warData;
+  } catch (error) {
+    console.error(
+      "Errore nel recupero dati della War " + warTag + "\n ERROR MSG =",
+      error.response?.data || error.message
+    );
+  }
 }
 
 //filters all the wars of the CWL season to find the ones with the correspondent CLAN TAG
 //returns an array which contains the wars battled by the clan in the season
 async function warFilter(clanTag) {
-    let TaggedClanWars = []
-    let allSeasonWarTags = await getCurrentSeasonCWLWarTags(clanTag)
+  let correctClanWars = [];
+  let allSeasonWarTags = await getCurrentSeasonCWLWarTags(clanTag);
+  //console.log('clantag to find: '+clanTag)
 
-        // Itera su ogni round (che è un oggetto)
-    for (let round of allSeasonWarTags) {
-        console.log(`Processando round ${round.roundNumber}`);
-            
-        // Itera sui warTags del round corrente
-        for (let warTag of round.warTags) {
-            console.log(warTag)
-            let war = await getWarData(warTag);
-        }
+  let clanTagToMatch = "#" + clanTag;
+  //console.log('clantag to match: '+clanTagToMatch)
+
+  // Iteration on every round (che è un oggetto)
+  for (let round of allSeasonWarTags) {
+    //console.log(`Processing round ${round.roundNumber}`);
+
+    // Iteration of concurrent round
+    for (let warTag of round.warTags) {
+      //console.log(warTag)
+      let warData = await getWarData(warTag);
+
+      //if clanTag is found inside the war, the data of the war is saved in correctClanWars
+      if (
+        warData.clan.tag === clanTagToMatch ||
+        warData.opponent.tag === clanTagToMatch
+      ) {
+        //console.log("clan found in war: "+warTag)
+        correctClanWars.push(warData);
+        break; // Go to next round
+      }
     }
+  }
+  return correctClanWars;
 }
-    
-    /*forEach.round{
-        for each wartag{
-            let war = getWarData(wartag)
-            
-            if (war.clan.tag || war.opponent.tag)==clanTag) {   
-                taggedClanWars.push(warData)
-                break // breaks the cycle and goes to the next round
-            }
-        }
-    }
-    return taggedClanWars
-    */  
-
-
-
-
 
 //input tag without '#', output encoded tag with #
 //the output is used for the official CoC API
 //in this way the tag can always be passed in it's normal form and then converted for each function if needed
-function transformTag(tag){
-    let transformedTag = encodeURIComponent('#' + tag)
-    return transformedTag
+function transformTag(tag) {
+  let transformedTag = encodeURIComponent("#" + tag);
+  return transformedTag;
 }
 
+//TO STRINGS
 function warTagsToString(warTagsData) {
-    //console.log("toString Called")
-    let result = "=== CWL WAR TAGS ===\n";
-  
-    warTagsData.forEach(round => {
+  let result = "=== CWL WAR TAGS ===\n";
+
+  warTagsData.forEach((round) => {
     result += `\n📅 ROUND ${round.roundNumber}:\n`;
-    
+
     if (round.warTags.length > 0) {
       round.warTags.forEach((warTag, warIndex) => {
         result += `   Guerra ${warIndex + 1}: ${warTag}\n`;
@@ -189,22 +190,18 @@ function warTagsToString(warTagsData) {
       result += "   Nessuna guerra in questo round\n";
     }
   });
-  
+
   return result;
 }
 
-
+function correctClanWarsToString(CorrectClanWars) {
+  let result = "=== Correct Clan Wars ===\n";
+}
 
 async function main() {
-  warFilter(C_ItalianArmyTag)
-  //getWarData('8QQVQ9LR2') 
-
-
   //let warTagsData = await getCurrentSeasonCWLWarTags(C_ItalianArmyTag);
   //console.log(warTagsToString(warTagsData))
-    
+
+  let correctClanWars = await warFilter(C_ItalianArmyTag);
 }
-main()
-
-//chiamate di prova
-
+main();

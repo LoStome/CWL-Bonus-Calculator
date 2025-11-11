ad098f9a81342087f988c851dd26f9d2831f7f71
Stome <lor.stomeo@gmail.com>
2025-11-11 02:43:22 +0100

    Update coc.js

--- coc.js
+++ coc.js
@@ -59,17 +59,15 @@ async function showPlayerInfo(playerTag) {
 }
 
 //API KINGS
-/*
-async function ShowClanInfo(tag) {
-  try {
-    const clanInfo = await client.getClan(tag)
-    console.log("this is the clan info:", clanInfo)
 
+async function ShowPlayerInfoKING(tag) {
+  try {
+    let playerInfo = await clashKingClient.get(`/player/${tag}/stats`)(tag);
+    console.log("(CLASH KING) this is the player info:", playerInfo);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
-*/
 
 async function getCurrentSeasonCWLWarTags(clanTag) {
   try {
@@ -166,10 +164,85 @@ async function warFilter(clanTag) {
   return correctClanWars;
 }
 
-async function savePlayerData(clanTag) {
-  let correctClanWars = await correctClanWars(clanTag);
+async function savedPlayerData(clanTag) {
+  let correctClanWars = await warFilter(clanTag);
+  let clanTagToMatch = "#" + clanTag;
+  //console.log('clantag to match: '+clanTagToMatch)
+  let clanMembers = [];
 
   try {
+    //for each round
+    for (let i = 0; i < correctClanWars.length; i++) {
+      let warData = correctClanWars[i];
+
+      let members;
+      //console.log("\nWar N: " + (i + 1));
+      //checks which is the correct clan from where to save player data for this round
+      if (warData.war.clan.tag === clanTagToMatch) {
+        members = warData.war.clan.members;
+      } else {
+        members = warData.war.opponent.members;
+      }
+
+      //starts to save each player as an object
+      for (let j = 0; j < members.length; j++) {
+        let member = members[j];
+
+        let memberData = {
+          tag: member.tag,
+          name: member.name,
+          townhallLevel: member.townhallLevel,
+          mapPosition: member.mapPosition,
+          attacks: member.attacks || [],
+          /*
+          defences not implemented
+          opponentAttacks: member.opponentAttacks || 0,
+          bestOpponentAttack: member.bestOpponentAttack || null
+          */
+        };
+
+        // to checks if player already exists, saves member index
+        let existingIndex = clanMembers.findIndex(
+          (member) => member.tag === memberData.tag
+        );
+
+        //console.log("Player N " + (j + 1) + " = " + member.name);
+        //checks if the player already exists with it's tag
+        if (existingIndex === -1) {
+          //modifies the attacks property of memberData
+          memberData.attacks = (member.attacks || []).map((attack) => ({
+            ...attack, //copies attack properties
+            warTag: warData.warTag, //adds war's wartag
+            warNumber: i + 1, //adds war's number
+          }));
+          //adds player to the list
+          clanMembers.push(memberData);
+          /*console.log(
+            "New member added: " +
+              member.name +
+              ": added  " +
+              (member.attacks?.length || 0) +
+              " attacks\n"
+          );*/
+        } else {
+          //adds war attacks to the already added player
+          let attacksWithWarInfo = (member.attacks || []).map((attack) => ({
+            ...attack,
+            warTag: warData.warTag,
+            warNumber: i + 1,
+          }));
+
+          clanMembers[existingIndex].attacks.push(...attacksWithWarInfo);
+          /*
+          console.log(
+            "Already existing member, added " +
+              (member.attacks?.length || 0) +
+              " attacks\n"
+          );
+          */
+        }
+      }
+    }
   } catch (error) {
     console.error(
       "Errore durante il salvataggio dei dati dei giocatori:",
@@ -177,6 +250,7 @@ async function savePlayerData(clanTag) {
     );
     return [];
   }
+  return clanMembers;
 }
 
 //input tag without '#', output encoded tag with #
@@ -218,11 +292,63 @@ function correctClanWarsToString(correctClanWars) {
   return result;
 }
 
+function savedPlayerDataToString(clanMembers) {
+  let result = "=== CLAN MEMBERS DATA ===\n";
+
+  for (let i = 0; i < clanMembers.length; i++) {
+    let member = clanMembers[i];
+    result += `Member ${i + 1}: ${member.name} (${member.tag})\n`;
+    result += `  Townhall: ${member.townhallLevel}, Position: ${member.mapPosition}\n`;
+    result += `  Total Attacks: ${member.attacks.length}\n`;
+
+    // Calcola le stelle totali con ciclo for
+    let totalStars = 0;
+    for (let j = 0; j < member.attacks.length; j++) {
+      totalStars += member.attacks[j].stars;
+    }
+    result += `  Total Stars: ${totalStars}\n`;
+
+    // Raggruppa attacchi per guerra
+    let attacksByWar = {};
+    member.attacks.forEach((attack) => {
+      let warKey = `${attack.warNumber} (${attack.warTag})`;
+      if (!attacksByWar[warKey]) attacksByWar[warKey] = [];
+      attacksByWar[warKey].push(attack);
+    });
+
+    // Mostra attacchi per guerra con stelle per guerra
+    Object.keys(attacksByWar).forEach((warKey) => {
+      let warStars = 0;
+      for (let k = 0; k < attacksByWar[warKey].length; k++) {
+        warStars += attacksByWar[warKey][k].stars;
+      }
+      result += `  War ${warKey}: ${attacksByWar[warKey].length} attacks, ${warStars} stars\n`;
+      attacksByWar[warKey].forEach((attack) => {
+        result += `    vs ${attack.defenderTag}: ${attack.stars} stars, ${attack.destructionPercentage}%\n`;
+      });
+    });
+
+    result += "------------------------\n";
+  }
+
+  result += `Total members: ${clanMembers.length}\n`;
+  return result;
+}
+
+// MAIN:
+
 async function main() {
   //let warTagsData = await getCurrentSeasonCWLWarTags(C_ItalianArmyTag);
   //console.log(warTagsToString(warTagsData))
 
-  let correctClanWars = await warFilter(C_ItalianArmyTag);
-  console.log(correctClanWarsToString(correctClanWars));
+  //let correctClanWars = await warFilter(C_ItalianArmyTag);
+  //console.log(correctClanWarsToString(correctClanWars));
+  //let randomclan = "8PYC0G2Y";
+
+  let savedPlayerData1 = await savedPlayerData(C_ItalianArmyTag);
+  console.log(savedPlayerDataToString(savedPlayerData1));
+
+  //showPlayerInfo("2UPPCGV9");
+  //ShowPlayerInfoKING(P_Lore);
 }
 main();

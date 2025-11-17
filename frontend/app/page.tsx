6d78dd029a88009a0cc82e7b2723baf19038cf1e
Stome <lor.stomeo@gmail.com>
2025-11-17 03:35:37 +0100

    aggiunte prime pagine front end (next.js, react, tailwind)

---
+++ frontend/app/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+
+export default function HomePage() {
+  const [tag, setTag] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log("Clan tag:", tag);
+
+    // Later you will use this:
+    // const response = await fetch(`/api/clan?tag=${tag}`);
+
+    // For now, just log it.
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-4xl text-black font-bold mb-8">
+        CWL Bonus Calculator
+      </h1>
+
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-lg rounded-xl p-6 w-full max-w-md"
+      >
+        <label className=" text-black mb-2 block">Enter Clan Tag</label>
+
+        <input
+          type="text"
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+          placeholder="#...."
+          className="text-[#9d9d9d] font-clashSmall w-full border p-3 rounded-lg mb-4 outline-none focus:ring-2 focus:ring-blue-400"
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-red-700 transition"
+        >
+          Search Clan...
+        </button>
+      </form>
+    </div>
+  );
+}
